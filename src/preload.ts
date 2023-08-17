07c5b39610444df27f1734fdaf94d53bcfeabd3e
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2023-08-17 09:20:15 -0300

    Merge pull request #13 from camilocoelhogomes:featBackEnd/createScenario FeatBackEnd/createScenario

--- src/preload.ts
+++ src/preload.ts
@@ -7,4 +7,5 @@ contextBridge.exposeInMainWorld('api', {
   listProject: (args: unknown) => ipcRenderer.invoke('listProject', args),
   createProject: (args: unknown) => ipcRenderer.invoke('createProject', args),
   getProject: (args: unknown) => ipcRenderer.invoke('getProject', args),
+  createScenario: (args: unknown) => ipcRenderer.invoke('createScenario', args),
 });
